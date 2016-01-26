9a4e5c31a062b37c7f173fa6b43a5999174fbc91
John Marinelli <jonmarinelli@hotmail.com>
2016-01-26 10:39:52 -0700

    ruby vs raw sql

--- benchmark.rb
+++ benchmark.rb
@@ -3,19 +3,42 @@ require_relative 'models'
 
 p 'Measuring.....'
 
+# No eager loading
 Benchmark.bm(10) do |x|
-  x.report {
+  x.report("W/o eager loading") {
     u = User.first
     ps = u.posts
     ps.each { |p| p.body }
   }
 end
 
-
+# Eager loading
+# performs about 700% quicker than not eager loading
 Benchmark.bm(10) do |x|
-  x.report {
-    u = User.where(:id => 1).includes(:posts)
-    ps = u.first.posts
+  x.report("W/ eager loading") {
+    u = User.where(:id => 1)
+            .includes(:posts)
+            .first
+    ps = u.posts
     ps.each { |p| p.body }
   }
 end
+
+# Rubyesque way
+Benchmark.bm(10) do |x|
+  x.report("Using Ruby") {
+    User.where(:id => 1)
+        .includes(:posts)
+        .map { |u| u.posts.count }
+        .reduce :+
+  }
+end
+
+# Raw SQL 
+# performs about 200% quicker
+Benchmark.bm(10) do |x|
+  x.report("Using SQL") {
+    User.count_by_sql ["SELECT COUNT(*) FROM users JOIN posts ON users.id = posts.user_id WHERE users.id = 1"]
+  }
+end
+
